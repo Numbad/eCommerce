feba45322602891824bc57955e1516f5ad4de8e4
DIOP <adama.diop@capgemini.com>
2017-11-03 09:41:28 +0100

    manga img view added, app made responsive

--- public/manga-details/manga-details.component.js
+++ public/manga-details/manga-details.component.js
@@ -4,7 +4,7 @@ angular.module('mangaDetails')
     .component('mangaDetails', {
         templateUrl: 'manga-details/manga-details.template.html',
         controllerAs: "mdc",
-        controller: ['$routeParams', 'Manga', '$scope', '$window',  
+        controller: ['$routeParams', 'Manga', '$scope', '$window',
             function MangaDetailsController($routeParams, Manga, $scope, $window) {
                 var self = this;
                 Manga.getManga($routeParams.mangaId)
@@ -15,8 +15,19 @@ angular.module('mangaDetails')
                     .catch(function onError() {
                         console.log("Erreur de récupération du manga : " + $routeParams.mangaId);
                     });
-                self.submit = function() {
-                    $window.location = '#!/validate';
+                self.submit = function () {
+                    if ($window.localStorage.getItem("basket") == null) {
+                        var a = [];
+                        a.push($scope.manga);
+                        $window.localStorage.setItem("basket", JSON.stringify(a));
+                    }
+                    else {
+                        var valu = JSON.parse($window.localStorage.getItem("basket"));
+                        valu.push($scope.manga);
+                        $window.localStorage.setItem("basket", JSON.stringify(valu));
+                    }
+                    console.log(JSON.parse($window.localStorage.getItem("basket")));
+
                 }
             }]
     });
