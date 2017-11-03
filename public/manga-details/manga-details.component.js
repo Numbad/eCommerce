8bf35b35a7655be0bf09b8ae01b5df2bf984f68f
Altamore Quentin <quentin.altamore@estiam.com>
2017-11-03 16:59:34 +0100

    update localStorage object type given

--- public/manga-details/manga-details.component.js
+++ public/manga-details/manga-details.component.js
@@ -17,21 +17,18 @@ angular.module('mangaDetails')
                     
                 Manga.getManga($routeParams.mangaId)
                     .then(function onSuccess(response) {
-                        //console.log(response.data.manga[0]);
                         $scope.manga = response.data.manga[0];
                     })
                     .catch(function onError() {
                         console.log("Erreur de récupération du manga : " + $routeParams.mangaId);
                     });
                 
-                self.addShoppingCart = function(name, tome) {
-                    console.log("Name : " + name + " & Tome : " + tome);
-                    if(name.length != 0 && tome > 0) {
-                        var manga = {"name": name, "tome": tome};
+                self.addShoppingCart = function(manga, tome) {
+                    if(manga.name.length != 0 && tome > 0) {
                         self.shoppingList.push(manga);
                         localStorage.setItem("shoppingList", JSON.stringify(self.shoppingList));
 
-                        toaster.pop('success', name + ' - Tome ' + tome + ' a été ajouté au panier');
+                        toaster.pop('success', manga.name + ' - Tome ' + tome + ' a été ajouté au panier');
                     }
                 }
             }]
