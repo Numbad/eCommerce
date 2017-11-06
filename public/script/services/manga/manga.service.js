99b727b9d81d444d1740a3336f16ff23d7e0107d
DIOP <adama.diop@capgemini.com>
2017-11-06 19:13:03 +0100

    api links changed

--- public/script/services/manga/manga.service.js
+++ public/script/services/manga/manga.service.js
@@ -10,11 +10,11 @@ angular.module('services.manga')
             return service;
 
             function getAll() {
-                return $http.get("http://localhost:3000/mangas");
+                return $http.get("https://radiant-thicket-74699.herokuapp.com/mangas");
             }
 
             function getMangaById(mangaId) {
-                return $http.get("http://localhost:3000/mangas/" + mangaId);
+                return $http.get("https://radiant-thicket-74699.herokuapp.com/mangas" + mangaId);
             }
 
         }
