7805426328ce01b6c813326bd15c78b48a2d3a86
Altamore Quentin <quentin.altamore@estiam.com>
2017-11-07 16:31:44 +0100

    modification de l'extension des fichiers jpeg en jpg

--- public/script/services/manga/manga.service.js
+++ public/script/services/manga/manga.service.js
@@ -10,10 +10,12 @@ angular.module('services.manga')
             return service;
 
             function getAll() {
+                //return $http.get("http://localhost:3000/mangas");
                 return $http.get("https://radiant-thicket-74699.herokuapp.com/mangas");
             }
 
             function getMangaById(mangaId) {
+                //return $http.get("http://localhost:3000/mangas/" + mangaId);
                 return $http.get("https://radiant-thicket-74699.herokuapp.com/mangas/" + mangaId);
             }
 
